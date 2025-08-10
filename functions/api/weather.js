94d0ffe17f041ed13e98df3bd2562f5e7f3aca6b
Anna Jouppi <anna.jouppi@gmail.com>
2025-08-10 11:50:51 +0300

    updated weather.js

--- functions/api/weather.js
+++ functions/api/weather.js
@@ -1,30 +1,30 @@
 export async function onRequestGet(context) {
-    const { request, env } = context;
-    const url = new URL(request.url);
-    const location = url.searchParams.get('q');
-
-if (!location) {
-        return new Response('Location query parameter "q" is required.', {
-            status: 400,
-        });
-    }
-
-    const apiKey = env.WEATHER_API_KEY;
-    const weatherUrl = `https://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${encodeURIComponent(location)}`;
-
-    try {
-        const response = await fetch(weatherUrl);
-        const data = await response.json();
-       
-        return new Response(JSON.stringify(data), {
-            headers: {
-                'Content-Type': 'application/json',
-                'Access-Control-Allow-Origin': '*',
-            },
+  const { request, env } = context;
+  const url = new URL(request.url);
+  const location = url.searchParams.get('q');
+  
+  if (!location) {
+    return new Response('Location query parameter "q" is required.', { 
+      status: 400 
     });
-    } catch (error) {
-        return new Response('Error fetching weather data.', {
-            status: 500,     
-        });
-    }
+  }
+  
+  const apiKey = env.WEATHER_API_KEY;
+  const weatherUrl = `https://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${encodeURIComponent(location)}`;
+  
+  try {
+    const response = await fetch(weatherUrl);
+    const data = await response.json();
+    
+    return new Response(JSON.stringify(data), {
+      headers: {
+        'Content-Type': 'application/json',
+        'Access-Control-Allow-Origin': '*'
+      }
+    });
+  } catch (error) {
+    return new Response('Error fetching weather data.', { 
+      status: 500 
+    });
+  }
 }
